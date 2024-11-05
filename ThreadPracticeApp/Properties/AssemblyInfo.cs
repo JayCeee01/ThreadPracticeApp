99449bf7a4bec834ab553ccf6ce7c97fde9706f3
JayCee <158656310+JayCeee01@users.noreply.github.com>
2024-11-05 22:47:03 +0800

    Add project files.

---
+++ ThreadPracticeApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ThreadPracticeApp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ThreadPracticeApp")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3e2916a6-3992-47b5-bb4e-9fa75800d3d1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ThreadPracticeApp/frmTrackThread.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+
+namespace ThreadPracticeApp
+{
+    public partial class frmTrackThread : Form
+    {
+        public frmTrackThread()
+        {
+            InitializeComponent();
+        }
+        public void MyThread()
+        {
+            label1.Text = "-Thread Starts-";
+            Console.WriteLine("-Thread Starts-");
+
+            Thread threadA = new Thread(new ThreadStart(MyThreadClass.Thread1));
+
+            Thread threadB = new Thread(new ThreadStart(MyThreadClass.Thread2));
+
+            Thread threadC = new Thread(new ThreadStart(MyThreadClass.Thread1));
+
+            Thread threadD = new Thread(new ThreadStart(MyThreadClass.Thread2));
+
+            threadA.Priority = ThreadPriority.Highest;
+            threadA.Name = "Thread A Process";
+
+            threadB.Priority = ThreadPriority.Normal;
+            threadB.Name = "Thread B Process";
+
+            threadC.Priority = ThreadPriority.AboveNormal;
+            threadC.Name = "Thread C Process";
+
+            threadD.Priority = ThreadPriority.BelowNormal;
+            threadD.Name = "Thread D Process";
+
+            threadA.Start();
+            threadB.Start();
+            threadC.Start();
+            threadD.Start();
+
+            threadA.Join();
+            threadB.Join();
+            threadC.Join();
+            threadD.Join();
+
+            Console.WriteLine("-End of Thread-");
+            label1.Text = "-End of Thread-";
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MyThread();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
